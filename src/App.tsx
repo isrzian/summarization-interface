d359883df23b2387eab33495ef2b97e7b7d43b2b
isrzian <shadow-sergej@ya.ru>
2023-05-13 11:29:02 +0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import axios from 'axios';
+import {Breadcrumb, Button, Col, Input, Layout, Menu, Row, theme} from 'antd';
 import './App.css';
+import {mockArticle} from './mock/MockArticle';
+
+const { Header, Content, Footer } = Layout;
+const { TextArea } = Input;
 
 function App() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [sendText, setSendText] = useState(mockArticle);
+    const [responseText, setResponseText] = useState('');
+
+    const {
+        token: { colorBgContainer },
+    } = theme.useToken();
+
+    async function createPromiseQuery(data: string) {
+        return axios.post(
+            'https://api-inference.huggingface.co/models/sambydlo/bart-large-scientific-lay-summarisation',
+            {inputs: data},
+            {
+
+                headers: {
+                    'Authorization': `Bearer ${process.env.REACT_APP_TOKEN}`,
+                    'Access-Control-Allow-Headers': 'Authorization',
+                    'content-type': 'application/json'
+                }
+            }
+        );
+    }
+
+    const generate = async () => {
+        setIsLoading(true);
+
+        if (sendText.length < 1) {
+            setIsLoading(false);
+            return;
+        }
+
+        const result = await createPromiseQuery(sendText)
+            // .then(({data}) => data)
+            // .catch(() => {
+            //     alert('Возникла ошибка!');
+            //     setIsLoading(false);
+            // });
+        console.log('result', result)
+
+        setResponseText(result.data.summary_text);
+
+        setIsLoading(false);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Layout className="layout">
+            <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    items={[{
+                        key: 1,
+                        label: 'Тестовый образец нейросети суммаризации научных текстов'
+                    }]}
+                />
+            </Header>
+            <Content style={{ padding: '0 50px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                </Breadcrumb>
+                <div className="site-layout-content" style={{ background: colorBgContainer }}>
+                    <Row gutter={[16, 16]}>
+                        <Col span={8}>
+                            <TextArea
+                                style={{ height: 500, marginBottom: 24 }}
+                                placeholder="Исходный текст"
+                                onChange={(data) => setSendText(data.target.value)}
+                                value={sendText}
+                            />
+                        </Col>
+                        <Col
+                            span={6}
+                            style={{ display: 'flex', justifyContent: 'center'}}
+                        >
+                            <Button
+                                size='large'
+                                onClick={generate}
+                                loading={isLoading}
+                            >
+                                Сгенерировать
+                            </Button>
+                        </Col>
+                        <Col span={8}>
+                            <TextArea
+                                style={{ height: 500, marginBottom: 24 }}
+                                placeholder="Сгенерированная суммаризация"
+                                value={responseText}
+                            />
+                        </Col>
+                    </Row>
+                </div>
+            </Content>
+            <Footer style={{ textAlign: 'center' }}>СФУ ©2023 Научная библиотека</Footer>
+        </Layout>
     </div>
   );
 }
---
+++ src/mock/MockArticle.ts
@@ -0,0 +1,15 @@
+export const mockArticle = 'This paper elucidates the relationship between possible changes in volunteering experienced by older people during the COVID-19 pandemic, and their motivation to volunteer, as well as\n' +
+    'the direct or indirect experience of COVID-19 symptoms. Given the well-known positive benefits\n' +
+    'of volunteering in older age both for individuals (in terms of improved health and wellbeing) and\n' +
+    'society at large, there is a paucity of studies on older volunteers in the time of COVID-19. In this\n' +
+    'context, older people’s volunteering was highly challenged due to age-based physical and social\n' +
+    'restrictions put in place by national governments, which have been considered as ageist by a large\n' +
+    'part of the gerontological scientific community. This study was carried out on a sample of 240 Italian\n' +
+    'older volunteers. The results suggest that during the COVID-19 pandemic, especially older volunteers\n' +
+    'driven by social goals (e.g., opportunities to have relationships with others) were able to continue\n' +
+    'volunteer activities without needing to change them. The study also clarified that having directly\n' +
+    'or indirectly experienced COVID-19 symptoms did not influence changes in voluntary activities of\n' +
+    'older people. These results have important policy implications, given the indication that through\n' +
+    'volunteering, older individuals may try to counter the undesired calls by the governments for selfisolation and physical distancing. It is important that in emergency situations involving older people,\n' +
+    'policy makers should not treat them as only recipients of health and social care, but also as useful\n' +
+    'providers of help in the community';
